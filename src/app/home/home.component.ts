cc355960bbe73e9d163d44da5d390033eb69e508
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2023-09-12 20:11:40 +0630

    weather icons, and images  changed according to the temperature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent {
   // FIRST THING FIRST, INJECT HTTP 
   constructor(private http: HttpClient) { }
-  city: string = 'Mandalay';
-  temp: string = '26.7';
+  city: string = 'Alaska';
+  temp: number = 26.7;
   main: string = '';
   desc: string = 'overcast clouds';
-  icon: string = '';
+  icon: boolean = true;
+  icon_path: string = 'wi-owm-800';
   speed: string = '3.07';
   country: string = 'MM';
   visibility: string = '10000';
   humidity: string = '50';
+
+  error_txt: string = 'error text appears here!';
+
+  bg_img: string = '../../assets/img/cloudy.jpg';
 
   getWeather() {
     //1. API Call
@@ -30,16 +35,65 @@ export class HomeComponent {
     result.subscribe((data: any) => {
       this.city = data['name'];
       this.temp = data['main']['temp'];
+      this.humidity = data['main']['humidity'];
       this.main = data['weather'][0]['main'];
       this.desc = data['weather'][0]['description'];
-      this.icon = data['weather'][0]['icon'];
       this.speed = data['wind']['speed'];
       this.country = data['sys']['country'];
       this.visibility = data['visibility'];
-      this.humidity = data['weather'][0]['humidity'];
+
+
+      // error_txt MSG 
+      this.error_txt = data['cod'];
+      console.log(this.error_txt);
+      if (this.error_txt != '404') {
+        this.error_txt = "sorry soory sorry sorry"
+      }
+      console.log(this.error_txt);
+
+
+      console.log(this.main);
+
+    //GET IMAGE & WEATHER ICON
+    if (this.temp <= 16) {
+      this.bg_img = "../../assets/img/snowy.jpg";
+      this.icon_path = "wi-snow";
+      // this.icon = true;
+      // this.icon.classList.add("wi-snow-wind");
+    }
+    else if (this.temp > 16 && this.temp <= 30) {
+      this.bg_img = "../../assets/img/rainy.jpg";
+      this.icon_path = "wi-night-sprinkle"
 
     }
+    else {
+      this.bg_img = "../../assets/img/sunny.jpg";
+      this.icon_path = "wi-day-sunny-overcast";
+      // "wi-day-sunny-overcast" "wi-owm-800"
+    }
+      
+      
+    }
     )
+    // GET ICON 
+    // wont work this way "document.getElementsByClassName('wi')"!!!!
+
+  //  let icon = document.getElementsByClassName('wi')[1];
+  //  if (this.temp <= 16) {
+  //    icon.classList.add('wi-snow-wind');
+  //    // this.icon.classList.add("wi-snow-wind");
+  //  }
+  //  else if (this.temp > 16 && this.temp <= 30) {
+  //    this.bg_img = "../../assets/img/rainy.jpg";
+  //    icon.classList.add('wi-rain-wind');
+  //  }
+  //  else {
+  //    this.bg_img = "../../assets/img/sunny.jpg";
+  //    icon.classList.add('wi wi-night-sprinkle');
+  //  }
+
+ 
+
   }
 
 }
