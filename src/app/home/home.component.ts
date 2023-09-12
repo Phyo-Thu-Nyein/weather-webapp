190ef4496d9ed8b5071fa83ecd8bcc6d546ad1c9
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2023-09-12 12:39:06 +0630

    ui updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  // FIRST THING FIRST, INJECT HTTP 
   constructor(private http: HttpClient) { }
-  city: string = '';
+  city: string = 'Mandalay';
+  temp: string = '26.7';
+  main: string = '';
+  desc: string = 'overcast clouds';
+  icon: string = '';
+  speed: string = '3.07';
+  country: string = 'MM';
+  visibility: string = '10000';
+  humidity: string = '50';
 
   getWeather() {
     //1. API Call
@@ -20,7 +28,16 @@ export class HomeComponent {
 
     //3. Subscribe to it
     result.subscribe((data: any) => {
-      console.log(data['main']['temp']);
+      this.city = data['name'];
+      this.temp = data['main']['temp'];
+      this.main = data['weather'][0]['main'];
+      this.desc = data['weather'][0]['description'];
+      this.icon = data['weather'][0]['icon'];
+      this.speed = data['wind']['speed'];
+      this.country = data['sys']['country'];
+      this.visibility = data['visibility'];
+      this.humidity = data['weather'][0]['humidity'];
+
     }
     )
   }
